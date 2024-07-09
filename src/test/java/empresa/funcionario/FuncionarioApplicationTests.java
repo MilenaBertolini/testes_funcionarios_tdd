6f94857ef07913a8690ce580fb1c2f08c1708ab1
MilenaBertolini <milenaarantesb@gmail.com>
2024-07-09 18:49:42 -0300

    adicionando projeto e dependencias

---
+++ src/test/java/empresa/funcionario/FuncionarioApplicationTests.java
@@ -0,0 +1,13 @@
+package empresa.funcionario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuncionarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
