b2c5f7e387a09263ad7be3c4d82295359800c6fa
diegohmad <diego.diniz@estudante.iftm.edu.br>
2024-07-16 21:53:34 -0300

    Update FuncionarioTests.java

--- src/test/java/empresa/funcionario/FuncionarioTests.java
+++ src/test/java/empresa/funcionario/FuncionarioTests.java
@@ -8,7 +8,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class FuncionarioTests {
-    
     private Funcionario funcionario;
 
     @BeforeEach
@@ -126,27 +125,84 @@ public class FuncionarioTests {
 
         assertEquals(resultadoEsperado, resultadoObtido);
     }
+    @Test
+    @DisplayName("Casos de teste que verifica o nome válido do funcionário")
+    public void testarModificarNomeValido() {
 
-    private FuncionarioTerceirizado funcTerceirizado;
+        String nomeValido = "Teste";
+        String resultadoEsperado = "Teste";
 
-    @BeforeEach
-    public void instanciaFuncionarioTerceirizado(){
-        funcTerceirizado = new FuncionarioTerceirizado();
+        funcionario.setNomeFuncionario(nomeValido);
+        String resultadoObtido = funcionario.getNomeFuncionario();
+
+        assertEquals(resultadoEsperado, resultadoObtido);
+
     }
 
     @Test
-    @DisplayName("Caso de teste para construtor em caso de entrada de despesas inválidas")
-    public void testarConstrutorEntradaDespesaInválida(){
+    @DisplayName("Casos de teste que verifica horas trabalhadas inválidas")
+    public void testarModificarHorasTrabalhadasInvalido() {
 
-        Double despesasAdicionaisInvalidas = 1001.00;
+        int horasTrabalhadasInvalidas = 41;
 
-        assertThrows(IllegalArgumentException.class,
-            () -> {
-                funcTerceirizado = new FuncionarioTerceirizado(despesasAdicionaisInvalidas);
-            }
-        );
+        assertThrows(IllegalArgumentException.class, () -> {
+            funcionario.setHorasTrabalhadas(horasTrabalhadasInvalidas);
+        });
 
     }
 
+    @Test
+    @DisplayName("Casos de teste que verifica horas trabalhadas válidas")
+    public void testarModificarHorasTrabalhadasValido() {
 
+        int horasTrabalhadasValidas = 37;
+        int resultadoEsperado = 37;
+
+        funcionario.setHorasTrabalhadas(horasTrabalhadasValidas);
+        int resultadoObtido = funcionario.getHorasTrabalhadas();
+
+        assertEquals(resultadoEsperado, resultadoObtido);
+
+    }
+
+    @Test
+    @DisplayName("Casos de teste que verifica o valor hora inválido no limite inferior do funcionário")
+    public void testarModificarValorHoraInvalidoLimiteInferior() {
+
+        // valor entre 52.8 e 132.00
+        Double valorHoraInvalida = 52.5;
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            funcionario.setValorHora(valorHoraInvalida);
+        });
+
+    }
+
+    @Test
+    @DisplayName("Casos de teste que verifica o valor hora inválido no limite superior do funcionário")
+    public void testarModificarValorHoraInvalidoLimiteSuperior() {
+
+        // valor entre 52.8 e 132.00
+        Double valorHoraInvalida = 132.01;
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            funcionario.setValorHora(valorHoraInvalida);
+        });
+
+    }
+
+    @Test
+    @DisplayName("Casos de teste que verifica o valor hora válido do funcionário")
+    public void testarModificarValorHoraValido() {
+
+        // valor entre 52.8 e 132.00
+        Double horasTrabalhadasValidas = 90.2;
+        Double resultadoEsperado = 90.2;
+
+        funcionario.setValorHora(horasTrabalhadasValidas);
+        Double resultadoObtido = funcionario.getValorHora();
+
+        assertEquals(resultadoEsperado, resultadoObtido);
+
+    }
 }
